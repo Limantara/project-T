a486eb688c606c92dcabecd0b69b6d486dab1b5b
Edwin <edwin.limantara@yahoo.com>
2014-10-18 00:05:21 -0700

    change https

--- scripts/app.js
+++ scripts/app.js
@@ -6,9 +6,21 @@ if (!('webkitSpeechRecognition' in window)) {
 	recognition.interimResults = true;
 }
 
+recognition.onresult = function(event) {
+    var i;
+    return $('.content p').text('" ' + ( (function() {
+      var _i, _ref, _ref1, _results;
+      _results = [];
+      for (i = _i = _ref = event.resultIndex, _ref1 = event.results.length - 1; _i <= _ref1; i = _i += 1) {
+        _results.push(event.results[i][0].transcript);
+      }
+      return _results;
+    })()).join(''));
+  };
+
 $(document).ready(function() {
 	$('.button img').click(function() {
-		recognition.lang = 'en-US';
+		recognition.lang = 'id';
 		recognition.start();
 	});
 });
